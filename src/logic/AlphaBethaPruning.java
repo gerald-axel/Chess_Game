6e82aa07024391214fc58d1003c140220f7bf393
Miguel Grover <mickygalgro@hotmail.com>
2016-03-23 17:30:26 -0600

    Get the future moves

--- src/logic/AlphaBethaPruning.java
+++ src/logic/AlphaBethaPruning.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.util.ArrayList;
+
 import pieces.Piece;
 
 public class AlphaBethaPruning {
@@ -8,39 +10,69 @@ public class AlphaBethaPruning {
 		root = new NodeAlphaBetha(rootmap);
 		prune(root,-100000,100000,5,true);
 	}
-	public int prune(NodeAlphaBetha node, int alpha, int betha,int prof, boolean max){
+	public NodeAlphaBetha prune(NodeAlphaBetha node, int alpha, int betha,int prof, boolean max){
+		NodeAlphaBetha aux = null;
+		node = findFuture(node);
 		if(node.nodes.isEmpty() || prof==0){
 			node.calculateValue();
-			return node.getValue();
+			return node;
 		}else{
 			if(max){
 				for (NodeAlphaBetha sonNode : node.nodes) {
 					
-					int aux = prune(sonNode,alpha,betha,prof-1,false);
-					if(aux>alpha){
-						aux=alpha;
+					aux = prune(sonNode,alpha,betha,prof-1,false);
+					if(aux.getValue()>alpha){
+						alpha = aux.getValue();
 					}
 					if(alpha>=betha){
-						return betha;
+						aux.setValue(betha);
+						return aux;
 					}
 					
 				}
-				return alpha;
+				aux.setValue(alpha);
+				return aux;
 			}
 			else{
 				for (NodeAlphaBetha sonNode : node.nodes) {
 					
-					int aux = prune(sonNode,alpha,betha,prof-1,true);
-					if(aux<alpha){
-						aux=alpha;
+					aux = prune(sonNode,alpha,betha,prof-1,true);
+					if(aux.getValue()<betha){
+						betha = aux.getValue();
 					}
 					if(alpha>=betha){
-						return alpha;
+						aux.setValue(alpha);
+						return aux;
 					}
 					
 				}
-				return betha;
+				aux.setValue(betha);
+				return aux;
 			}
 		}
+	}
+	
+	public NodeAlphaBetha findFuture(NodeAlphaBetha node){
+		Piece[][] map = node.getMap();
+		
+		for(int x = 0; x < 8; x++){
+			for(int y = 0; y < 8; y++){
+				if(map[x][y]!=null){
+					
+					PossibleMoves.reviewPiece(map[x][y]);
+					for (javax.swing.JPanel piece : PossibleMoves.possibleMoves) {
+						Piece[][] aux = map.clone();
+						String []coord = piece.getName().split(",");
+						int newCoordenateX = Integer.parseInt(coord[0]);
+						int newCoordenateY = Integer.parseInt(coord[1]);
+						aux[newCoordenateX][newCoordenateY] = aux[x][y];
+						aux[x][y]=null;
+						node.nodes.add(new NodeAlphaBetha(aux));
+					}
+				}
+				
+			}
+		}
+		return node;
 	}
 }
